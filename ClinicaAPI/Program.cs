8f44dea8773ed56f34eae6775b9cb30221bdf2ff
DallBia <tibianchini.livros@gmail.com>
2024-01-05 03:37:50 -0300

    voltando

--- ClinicaAPI/Program.cs
+++ ClinicaAPI/Program.cs
@@ -79,8 +79,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowMyOrigin",
-        //builder => builder.WithOrigins("http://35.232.35.159")
-         builder => builder.WithOrigins("http://34.70.241.44")
+        builder => builder.WithOrigins("http://35.232.35.159")
+         //builder => builder.WithOrigins("http://34.70.241.44")
                           //builder => builder.WithOrigins("http://localhost:4200")
                           .AllowAnyMethod()
                           .AllowAnyHeader());
